f0e4dfa8c98779e0a4b17ba8db2df5e30d9f0127
lokesh-repaka <lokeshrepaka4@gmail.com>
2024-06-02 20:13:49 +0530

    project template

---
+++ template.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "requirements.py",
+    "setup.py",
+    "research/trials.ipynb",
+    "app.py"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the files {filename}")
+
+        if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+            with open(filepath ,"w") as f:
+                pass
+                logging.info(f"creating empty file:{filepath}")
+        else:
+            logging.info(f"{filename} is already exist")
+
+
