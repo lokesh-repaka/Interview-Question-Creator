fe4c7d15feb04073aed5e9ba519c9b5c7aa6f59e
lokesh-repaka <lokeshrepaka4@gmail.com>
2024-06-02 20:23:02 +0530

    project template

--- template.py
+++ template.py
@@ -2,34 +2,35 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 list_of_files = [
     "src/__init__.py",
     "src/helper.py",
     "src/prompt.py",
     ".env",
-    "requirements.py",
+    "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "app.py"
+    "app.py",
+
 ]
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir,filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
     
-
     if filedir!="":
-        os.makedirs(filedir,exist_ok=True)
+        os.makedirs(filedir, exist_ok= True)
         logging.info(f"Creating directory {filedir} for the files {filename}")
 
-        if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-            with open(filepath ,"w") as f:
-                pass
-                logging.info(f"creating empty file:{filepath}")
-        else:
-            logging.info(f"{filename} is already exist")
 
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath , "w") as f:
+            pass 
+            logging.info(f"Creating empty file : {filepath}")
 
+    else:
+        logging.info(f"{filename} is already exists")
